5f57dba5a650791bda243543f841fc3266907d01
Matt Desktop <matt123miller@hotmail.co.uk>
2017-10-03 22:21:08 +0100

    moved some stuff into json files, fiddled with method signatures and stuff.

--- name-generator/name_generator.py
+++ name-generator/name_generator.py
@@ -7,8 +7,18 @@ from generator import Generator
 
 class Name_Generator(Generator):
 
+    filepath = ""
+    json = ""
+
+    def __init__(self):
+        # Does anything need to go here?
+
     def setup(self, filepath):
         print(filepath)
+        # Get json with the filepath
+        json = filepath
+        return json
 
     def generate(self, json):
+        # Do stuff with the json
         print(json)
