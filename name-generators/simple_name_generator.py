d8eab0ddef3ad256eeb66c48b62ae594ce99a445
Matt Desktop <matt123miller@hotmail.co.uk>
2017-10-05 17:38:07 +0100

    Renamed files to be clearer, as in future this project will move to taking -arguments via CLI. This should help.

--- name-generators/simple_name_generator.py
+++ name-generators/simple_name_generator.py
@@ -7,13 +7,13 @@ sys.path.insert(0, '../')
 from generator import Generator
 
 
-class Name_Generator(Generator):
+class Simple_Name_Generator(Generator):
 
     json = ""
     delimiter = ""
 
-    #def __init__(self):
-        # Does anything need to go here?
+    # def __init__(self):
+    # Does anything need to go here?
 
     def setup(self, filepath, delimiter):
         # Get json with the filepath
@@ -23,12 +23,11 @@ class Name_Generator(Generator):
         return self.json
 
     def generate(self):
-        # Do stuff with the json
-        full_name = ""
-        i = 1
-        for element in self.json:
-            full_name += random.choice(self.json[element])
-            if i < len(self.json):
-                full_name += self.delimiter
-            i += 1
+        # Simple name generator just takes a first and second name out of the json file.
+
+        full_name = ''
+        for key, value in self.json.items():
+
+            full_name += random.choice(value) + ' '
+
         return full_name
